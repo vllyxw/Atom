9aa3d6384e4cbb11e7960d7d5fede14d5fcb2bdd
Victoria <victoriapansini661@gmail.com>
2023-06-23 21:18:50 -0300

    m

--- pagInicio/script.js
+++
@@ -1,5 +0,0 @@
-function initAutocomplete() {
-    var input = document.getElementById('location-input');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-  }
-  initAutocomplete();
